8ee9e23b1fdb30ef3a21d71f12082697da98f057
Sasha Graves <all0exa@gmail.com>
2024-08-07 17:19:49 +0200

    Button that unintentionally randomizes cards

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
+import {useState} from 'react';
 import {FlipCard} from './FlipCard'
 import {hobbies} from './constants'
 
 function App() {
+  
+  const [someState, setSomeState] = useState(false)
 
+  const toggleButton = () => {
+    setSomeState(prevState => !prevState);
+  }
+  
   return (
     <div>
       <div className="App">
       <h1 className="App-title">Choose random hobby</h1>
+      <button className='mt-2 mb-4 mx-auto rounded-sm	border-2 text-white	border-dashed border-lime-500 hover:border-lime-200 w-fit p-2 ' onClick={toggleButton}>{someState ? 'Hey sweetie!' : 'Touch me <3'}</button>
         <div className="App-content h-full">
           {Object.keys(hobbies)
               .map(value => ( [value, Math.random()] ))
